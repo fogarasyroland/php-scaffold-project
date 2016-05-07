6db612790e08dbb6c8f9b61111dd7b67d4bec722
Fogarasy Roland <fogarasy.roland@gmail.com>
2016-05-07 20:28:57 +0200

    sass dir moved to app dir, add sourcemaps to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload')
+    sourcemaps = require('gulp-sourcemaps');
 gulp.task('default', ['sass', 'sass:watch'], function() {
 
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./app/www/sass/**/*.scss')
+  return gulp.src('./app/sass/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./app/www/css'))
     .pipe(livereload());;
 });
 
 gulp.task('sass:watch', function () {
   livereload.listen();
-  gulp.watch('./app/www/sass/**/*.scss', ['sass']);
+  gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
