15113e3f7063ff98f77885a7377d2171f82e7993
Fogarasy Roland <fogarasy.roland@gmail.com>
2016-05-07 21:45:07 +0200

    add xdebug to php

--- app/www/index.php
+++ app/www/index.php
@@ -1,12 +1,12 @@
 <html>
 <head>
   <title>PHP Scaffold Project</title>
+  <meta charset="utf-8"/>
   <link rel="stylesheet" href="css/index.css"/>
 </head>
 <body>
   <h1>PHP Scaffold Project</h1>
 <?php
-sdfa;
 $database   = $user = $password = "project";
 $host       = "mysql";
 $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
